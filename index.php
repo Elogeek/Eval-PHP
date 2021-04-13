845669007aa07f18120bbf5d21a4eae3f7d810dc
elogeek <elodiechristin@gmail.com>
2021-04-13 16:49:32 +0200

    go

--- index.php
+++ index.php
@@ -1,26 +1,31 @@
 <?php
 require_once "Include.php";
 //if user 's email is already existing it is ok, if not, it is sent back to the home page to create a new one by exemple
-if(!empty($_POST)&&($_POST['pseudo']) && !empty($_POST['pseudo'])) {
+if(!empty($_POST)&&($_POST['email']) && !empty($_POST['email'])) {
     session_start();
-    $_SESSION["pseudo"] ===[$_POST['pseudo']];
-    header("location:tchat.php");
+    $_SESSION["email"] ===[$_POST['email']];
+    header("location:index.php");
 }
 
+//connect ==== btn connect barrer/ grisé
+//noconnect ==== btn connect activé
 
 
 ?>
+<div id="frieze">
+    <img id="img1" src="assets/img/frieze.jpg" alt="beaux chatons">
+</div>
+<div id="tchat">
 
-<!--ALL TCHAT here-->
-<form method="POST" action="index.php">
-    Email :
-    <label for="pseudo">
-        <input type="text" name="pseudo" id="pseudo"/><br>
-    </label>
-    Message :
-    <label for="message">
-        <textarea name="message" id="message"></textarea><br>
-    </label>
-    <input type="submit" name="submit" value="Envoyez votre message !" id="envoi" />
-</form>
+    <!--ALL TCHAT here-->
+    <form method="POST" action="index.php">
+        Message :
+        <label for="message">
+            <textarea name="message" id="message"></textarea><br>
+        </label>
+        <input type="submit" name="submit" value="Envoyez votre message !" id="envoi" />
+    </form>
+
+</div>
+
 

39d1e38e3f603b64ff90b155c1b9b75131529bb6
elogeek <elodiechristin@gmail.com>
2021-04-22 14:47:39 +0200

    envoie

---
+++ include.php
@@ -0,0 +1,9 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . "/DB/DB.php";
+require_once $_SERVER['DOCUMENT_ROOT'] . "/Entity/Message.php";
+require_once $_SERVER['DOCUMENT_ROOT'] . "/Entity/User.php";
+require_once $_SERVER['DOCUMENT_ROOT'] . "/Manager/MessageManager.php";
+require_once $_SERVER['DOCUMENT_ROOT'] . "/Manager/UserManager.php";
+
+
+
